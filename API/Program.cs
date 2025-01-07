cb075d5f08ef478587805febea95a01e1755d553
chakrapanikotnana <kchakrapani123@gmail.com>
2025-01-07 17:14:46 -0500

    End of Section

--- API/Program.cs
+++ API/Program.cs
@@ -6,16 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
+builder.Services.AddDbContext<DataContext>(opt =>
+{
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DatingAppConn"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
-
+app.UseCors(p => p.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
