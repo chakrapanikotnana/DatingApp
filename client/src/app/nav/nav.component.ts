8f7b6c23d956142e3b05516c575d1d62b8719ebd
chakrapanikotnana <kchakrapani123@gmail.com>
2025-01-10 16:42:10 -0500

    End of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,34 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { NgIf } from '@angular/common';
+import { NgIf, TitleCasePipe } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, NgIf, BsDropdownModule],
+  imports: [FormsModule, NgIf, BsDropdownModule, RouterLink, RouterLinkActive,TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   //private accService = inject(AccountService);
   model: any = {};
+  private router = inject(Router);
+  private toastrService = inject(ToastrService);
 
   constructor(public accountService: AccountService) {}
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
-        console.log(response);
+        this.router.navigateByUrl('/members');
       },
       error: (error) => {
-        console.log(error);
+        this.toastrService.error(error.error);
       },
     });
     //console.log(this.model);
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
